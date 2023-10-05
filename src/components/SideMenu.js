4bc31dae07a418b25e262dd15443eedb651c67df
Canialon <canialon666@gmail.com>
2023-10-05 16:17:31 +0100

    finish the side menu work. Add routers.

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,35 +1,36 @@
 import SideNav, { NavIcon, NavItem, NavText } from "@trendmicro/react-sidenav";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SideMenu(){
-    return <SideNav
-        onSelect={selected=>{
-            console.log(selected)
-        }}
-        >
-
+    const navigate = useNavigate();
+    return (
+        <SideNav onSelect={selected => {
+            console.log(selected);
+            navigate(selected);
+        }}>
             <SideNav.Toggle />
-            <SideNav.Nav defaultSelected="home">
-                <NavItem>
+            <SideNav.Nav>
+                <NavItem eventKey="home"> 
                     <NavIcon><i className='fa fa-fw fa-home'/></NavIcon>
                     <NavText>Home</NavText>
                 </NavItem>
-                <NavItem>
+                <NavItem eventKey="todolist"> 
                     <NavIcon><i className='fa-solid fa-list-ol'/></NavIcon>
                     <NavText>To Do list</NavText>
                 </NavItem>
-                <NavItem>
+                <NavItem eventKey="today">
                     <NavIcon><i className='fa-solid fa-sun'/></NavIcon>
                     <NavText>Today</NavText>
                 </NavItem>
-                <NavItem>
+                <NavItem eventKey="calendar">
                     <NavIcon><i className='fa-solid fa-calendar-days'/></NavIcon>
                     <NavText>Calendar</NavText>
                 </NavItem>
             </SideNav.Nav>
         </SideNav>
-
-
+    );
 }
 
 export default SideMenu;
